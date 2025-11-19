ecd401ce7811d9c076ee59552af5bd934454a3f2
kim-se-hooh <wwb8524@naver.com>
2025-11-19 09:45:02 +0900

    2025-11-19 DB_board_v1

---
+++ board-back/src/main/java/org/example/boardback/repository/board/BoardCategoryRepository.java
@@ -0,0 +1,8 @@
+
+package org.example.boardback.repository.board;
+
+import org.example.boardback.entity.board.BoardCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardCategoryRepository extends JpaRepository<BoardCategory, Long> {
+}
